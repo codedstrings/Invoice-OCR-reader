2b6d57d159e159ec8008c1bdab674dd89f071798
Mridhul ML <mridhulml@gmail.com>
2024-01-22 20:49:41 +0530

    OCR done by teseract.js CDN and text printed to console and alert()

--- script.js
+++ script.js
@@ -5,23 +5,12 @@ var loadFile = function (event) {
         performOCR(event.target.files[0]);
 };
 
-async function performOCR(imageFile) {
-    // Initialize Tesseract.js
-    const worker = createWorker({
-        logger: info => console.log(info),
-    });
 
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-
-    // Recognize text from the image
-    const { data: { text } } = await worker.recognize(imageFile);
-    console.log('Recognized Text:', text);
-
-    // Display the recognized text (you might want to update this based on your UI)
-    alert('Recognized Text:\n' + text);
-
-    // Terminate the worker
-    await worker.terminate();
+async function performOCR(imageFile){
+    const worker=await Tesseract.createWorker('eng')
+    const ret = await worker.recognize(imageFile);
+  console.log(ret.data.text);
+  alert(ret.data.text);
+  await worker.terminate();
 }
+  
