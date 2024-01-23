2ca24a60f7912b50e5198b7d0c12d756fd99fe75
codedstrings <mridhulml@gmail.com>
2024-01-23 12:23:22 +0530

    change: extracted text displayed inside text-view instead of alert

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var imageFile;
+var sessionComplete=false;
 const extractBtn = document.querySelector(".extract-btn");
-
+const textView=document.querySelector(".text-view");
+const progressWrapper=document.querySelector("progress-wrapper")
 var loadFile = function (event) {
   var image = document.getElementById("uploaded-img");
   image.src = URL.createObjectURL(event.target.files[0]);
   //loaded image stored in imageFile
   imageFile = event.target.files[0];
   console.log(imageFile)
-
+  //enable extract again when another file is uploaded
+  extractBtn.classList.remove("invisible");
+  let lastChild = textView.lastElementChild;
+  if(sessionComplete){
+    textView.removeChild(lastChild);
+  }
+  //reset session when another file uploaded
+  sessionComplete=false;
 };
 
 extractBtn.addEventListener("click", handleExtract);
@@ -15,6 +24,8 @@ function handleExtract() {
   //check if imagefile is loaded
   if(imageFile){
     performOCR(imageFile);
+    extractBtn.classList.add("invisible");
+    // progressWrapper.classList.remove("invisible");
   }
   else (
     alert("Upload image!")
@@ -31,6 +42,11 @@ async function performOCR(imageFile) {
   });
   const ret = await worker.recognize(imageFile);
   console.log(ret.data.text);
-  alert(ret.data.text);
+  // alert(ret.data.text);
+  const extractedText=document.createElement("div");
+  extractedText.innerText=ret.data.text;
+  textView.appendChild(extractedText);
+
+  sessionComplete=true;
   await worker.terminate();
 }
